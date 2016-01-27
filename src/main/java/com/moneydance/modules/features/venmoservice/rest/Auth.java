b7ae766c9e8008c01764a4e174ed7bb3d48036ff
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2016-01-27 11:56:07 -0500

    Refactor Auth so that it's less hardcoded.

--- src/main/java/com/moneydance/modules/features/venmoservice/rest/Auth.java
+++ src/main/java/com/moneydance/modules/features/venmoservice/rest/Auth.java
@@ -5,6 +5,9 @@ import java.io.Closeable;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.locks.Lock;
@@ -16,20 +19,20 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Auth implements Closeable {
   private static final String CLIENT_ID = "3472";
+  private static final InetSocketAddress REDIRECT_ADDRESS =
+      new InetSocketAddress("localhost", 54321);
 
   private static final URI AUTH_URI;
-  private static final String AUTH_RESPONSE;
+  private static final Path AUTH_RESPONSE;
 
   static {
     try {
       AUTH_URI = new URI("https://api.venmo.com/v1/oauth/authorize?" + "client_id=" + CLIENT_ID
           + "&scope=access_profile%20access_balance%20access_payment_history");
+      AUTH_RESPONSE = Paths.get(Auth.class.getResource("auth_response.html").toURI());
     } catch (URISyntaxException e) {
       throw new Error(e);
     }
-
-    AUTH_RESPONSE = new StringBuilder()
-        .append("<html><body>Success! Return to Moneydance.</body></html>").toString();
   }
 
   private final Lock lock = new ReentrantLock();
@@ -41,27 +44,30 @@ public class Auth implements Closeable {
     Desktop d = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
     try {
       if (d == null || !d.isSupported(Desktop.Action.BROWSE)) {
-        throw new Exception("Cannot open " + AUTH_URI);
+        throw new UnsupportedOperationException("Cannot open " + AUTH_URI);
       } else {
         lock.lock();
         try {
           numAuthorizations++;
 
           if (server == null) {
-            server = HttpServer.create(new InetSocketAddress("localhost", 54321), -1);
+            server = HttpServer.create(REDIRECT_ADDRESS, -1);
             server.createContext("/", ex -> {
               Optional<String> token = getAuthToken(ex.getRequestURI());
 
-              if (token.isPresent()) {
-                ex.sendResponseHeaders(200, AUTH_RESPONSE.length());
-                ex.getResponseBody().write(AUTH_RESPONSE.getBytes());
-              } else {
-                ex.sendResponseHeaders(404, 0);
+              try {
+                if (token.isPresent()) {
+                  byte[] response = Files.readAllBytes(AUTH_RESPONSE);
+                  ex.sendResponseHeaders(200, response.length);
+                  ex.getResponseBody().write(response);
+                } else {
+                  ex.sendResponseHeaders(404, 0);
+                }
+
+                ex.close();
+              } finally {
+                token.ifPresent(f::complete);
               }
-
-              ex.close();
-
-              token.ifPresent(f::complete);
             });
             server.start();
           }
