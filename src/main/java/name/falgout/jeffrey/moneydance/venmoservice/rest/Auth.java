37d39d58faf2406b813a418afdf0ab5203ddc6f7
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2016-01-27 16:17:54 -0500

    Refactor Auth to make it testable.

--- src/main/java/name/falgout/jeffrey/moneydance/venmoservice/rest/Auth.java
+++ src/main/java/name/falgout/jeffrey/moneydance/venmoservice/rest/Auth.java
@@ -5,6 +5,7 @@ import static name.falgout.jeffrey.moneydance.venmoservice.rest.VenmoClient.ERRO
 
 import java.awt.Desktop;
 import java.io.Closeable;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,6 +17,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.uri.UriComponent;
 
@@ -23,74 +25,73 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Auth implements Closeable {
   private static final String CLIENT_ID = "3472";
-  private static final InetSocketAddress REDIRECT_ADDRESS =
-      new InetSocketAddress("localhost", 54321);
-
-  private static final URI AUTH_URI;
-  private static final Path AUTH_RESPONSE;
+  static final InetSocketAddress REDIRECT_ADDRESS = new InetSocketAddress("localhost", 54321);
+  static final Path AUTH_RESPONSE;
 
   static {
     try {
-      AUTH_URI = new URI("https://api.venmo.com/v1/oauth/authorize?" + "client_id=" + CLIENT_ID
-          + "&scope=access_profile%20access_balance%20access_payment_history");
       AUTH_RESPONSE = Paths.get(Auth.class.getResource("auth_response.html").toURI());
     } catch (URISyntaxException e) {
       throw new Error(e);
     }
   }
 
+  private final URI authUri;
+
   private final Lock lock = new ReentrantLock();
   private HttpServer server;
   private int numAuthorizations = 0;
 
-  public CompletableFuture<String> authorize() {
-    CompletableFuture<String> f = new CompletableFuture<>();
-    Desktop d = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+  public Auth() {
+    this("https://api.venmo.com/v1/oauth/authorize");
+  }
+
+  Auth(String baseUri) {
+    authUri =
+        UriBuilder.fromUri(baseUri)
+            .queryParam("client_id", CLIENT_ID)
+            .queryParam("scope",
+                String.join(" ", "access_profile", "access_balance", "access_payment_history"))
+            .build();
+  }
+
+  CompletableFuture<String> captureAuthorization() {
+    CompletableFuture<String> token = new CompletableFuture<>();
+    lock.lock();
     try {
-      if (d == null || !d.isSupported(Desktop.Action.BROWSE)) {
-        throw new UnsupportedOperationException("Cannot open " + AUTH_URI);
-      } else {
-        lock.lock();
-        try {
-          numAuthorizations++;
+      numAuthorizations++;
+      if (server == null) {
+        server = HttpServer.create(REDIRECT_ADDRESS, -1);
+        server.createContext("/", ex -> {
+          MultivaluedMap<String, String> query = UriComponent.decodeQuery(ex.getRequestURI(), true);
 
-          if (server == null) {
-            server = HttpServer.create(REDIRECT_ADDRESS, -1);
-            server.createContext("/", ex -> {
-              MultivaluedMap<String, String> query =
-                  UriComponent.decodeQuery(ex.getRequestURI(), true);
+          try {
+            if (query.containsKey(ACCESS_TOKEN) || query.containsKey(ERROR)) {
+              byte[] response = Files.readAllBytes(AUTH_RESPONSE);
+              ex.sendResponseHeaders(200, response.length);
+              ex.getResponseBody().write(response);
+            } else {
+              ex.sendResponseHeaders(404, 0);
+            }
 
-              try {
-                if (query.containsKey(ACCESS_TOKEN) || query.containsKey(ERROR)) {
-                  byte[] response = Files.readAllBytes(AUTH_RESPONSE);
-                  ex.sendResponseHeaders(200, response.length);
-                  ex.getResponseBody().write(response);
-                } else {
-                  ex.sendResponseHeaders(404, 0);
-                }
-
-                ex.close();
-              } finally {
-                if (query.containsKey(ACCESS_TOKEN)) {
-                  f.complete(query.getFirst(ACCESS_TOKEN));
-                } else if (query.containsKey(ERROR)) {
-                  f.completeExceptionally(new Exception(query.getFirst(ERROR)));
-                }
-              }
-            });
-            server.start();
+            ex.close();
+          } finally {
+            if (query.containsKey(ACCESS_TOKEN)) {
+              token.complete(query.getFirst(ACCESS_TOKEN));
+            } else if (query.containsKey(ERROR)) {
+              token.completeExceptionally(new Exception(query.getFirst(ERROR)));
+            }
           }
-        } finally {
-          lock.unlock();
-        }
-
-        d.browse(AUTH_URI);
+        });
+        server.start();
       }
     } catch (Exception e) {
-      f.completeExceptionally(e);
+      token.completeExceptionally(e);
+    } finally {
+      lock.unlock();
     }
 
-    f.whenComplete((r, t) -> {
+    token.whenComplete((r, t) -> {
       lock.lock();
       try {
         numAuthorizations--;
@@ -102,7 +103,25 @@ public class Auth implements Closeable {
       }
     });
 
-    return f;
+    return token;
+  }
+
+  public CompletableFuture<String> authorize() {
+    Desktop d = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+    if (d == null || !d.isSupported(Desktop.Action.BROWSE)) {
+      CompletableFuture<String> ex = new CompletableFuture<>();
+      ex.completeExceptionally(new UnsupportedOperationException("Cannot open " + authUri));
+      return ex;
+    } else {
+      CompletableFuture<String> auth = captureAuthorization();
+      try {
+        d.browse(authUri);
+      } catch (IOException e) {
+        auth.completeExceptionally(e);
+      }
+
+      return auth;
+    }
   }
 
   @Override
---
+++ src/test/java/name/falgout/jeffrey/moneydance/venmoservice/rest/AuthTest.java
@@ -0,0 +1,113 @@
+package name.falgout.jeffrey.moneydance.venmoservice.rest;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class AuthTest {
+  Auth auth;
+
+  @Before
+  public void before() {
+    auth = new Auth("http://localhost:12345");
+  }
+
+  @After
+  public void after() {
+    auth.close();
+  }
+
+  @Test
+  public void closeStopsServer() throws IOException {
+    auth.captureAuthorization();
+
+    try {
+      HttpServer.create(Auth.REDIRECT_ADDRESS, -1);
+      fail("Expected exception");
+    } catch (IOException e) {
+    }
+
+    auth.close();
+
+    HttpServer s = HttpServer.create(Auth.REDIRECT_ADDRESS, -1);
+    s.stop(0);
+  }
+
+  @Test
+  public void completingFutureStopsServer() throws IOException {
+    CompletableFuture<String> token = auth.captureAuthorization();
+
+    try {
+      HttpServer.create(Auth.REDIRECT_ADDRESS, -1);
+      fail("Expected exception");
+    } catch (IOException e) {
+    }
+
+    token.complete("foo");
+
+    HttpServer s = HttpServer.create(Auth.REDIRECT_ADDRESS, -1);
+    s.stop(0);
+  }
+
+  @Test
+  public void capturingAccessToken()
+    throws MalformedURLException, IOException, InterruptedException, ExecutionException {
+    CompletableFuture<String> token = auth.captureAuthorization();
+    URI auth = UriBuilder.fromUri("http://localhost")
+        .host(Auth.REDIRECT_ADDRESS.getHostName())
+        .port(Auth.REDIRECT_ADDRESS.getPort())
+        .queryParam(VenmoClient.ACCESS_TOKEN, "foo")
+        .build();
+
+    checkAuthResponse(auth.toURL().openStream());
+    assertEquals("foo", token.get());
+  }
+
+  private void checkAuthResponse(InputStream in) throws IOException {
+    ByteArrayOutputStream sink = new ByteArrayOutputStream();
+    byte[] buf = new byte[1024];
+    int numRead;
+    while ((numRead = in.read(buf)) > 0) {
+      sink.write(buf, 0, numRead);
+    }
+
+    byte[] allRead = sink.toByteArray();
+    assertArrayEquals(Files.readAllBytes(Auth.AUTH_RESPONSE), allRead);
+  }
+
+  @Test
+  public void capturingAccessTokenError()
+    throws MalformedURLException, IOException, InterruptedException, ExecutionException {
+    CompletableFuture<String> token = auth.captureAuthorization();
+    URI auth = UriBuilder.fromUri("http://localhost")
+        .host(Auth.REDIRECT_ADDRESS.getHostName())
+        .port(Auth.REDIRECT_ADDRESS.getPort())
+        .queryParam(VenmoClient.ERROR, "ruh roh")
+        .build();
+
+    checkAuthResponse(auth.toURL().openStream());
+    try {
+      token.get();
+    } catch (ExecutionException e) {
+      Throwable cause = e.getCause();
+      assertEquals("ruh roh", cause.getMessage());
+    }
+  }
+}
